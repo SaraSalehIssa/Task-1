40c5b417828414c22686e415e4dec457259cd5ad
SaraIssa <issasalehsara2001@gmail.com>
2024-03-16 20:32:15 +0200

    add css code Task #1

--- src/components/Sections/Section6.jsx
+++ src/components/Sections/Section6.jsx
@@ -6,9 +6,9 @@ function Section6() {
      <div className="container text-center py-5 mt-5">
         <h2 className='title-2 my-4 w-100'>A Price To Suit Everyone</h2>
         <p className='description-2 description-5 mx-auto'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus </p>
-        <p className='price w-100'>$40</p>
-        <p>UI Design Kit</p>
-        <p>See, One price. Simple.</p>
+        <p className='price w-100 mb-1'>$40</p>
+        <p className='design w-100'>UI Design Kit</p>
+        <p className='one-price w-100 mt-5 mb-1'>See, One price. Simple.</p>
         <button className="btn home-page-btn align-content-center align-items-center rounded-1 m-0" type="submit">Purchase now</button>
      </div>
     </div>
